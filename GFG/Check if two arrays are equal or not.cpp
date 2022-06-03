500c22ec482c3ddafa7a4c9de1f738f33b8b1d81
Yash Lalwani <77988031+lalwaniyash@users.noreply.github.com>
2022-06-03 15:13:06 +0000

    Solved "Check if two arrays are equal or not" from GFG

---
+++ GFG/Check if two arrays are equal or not.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+
+    //Function to check if two arrays are equal or not.
+    bool check(vector<ll> A, vector<ll> B, int N) {
+        unordered_map<ll, ll>mp1;
+        unordered_map<ll, ll>mp2;
+        for(int i = 0; i < N; i++){
+            mp1[A[i]]++;
+        }
+        
+        for(int i = 0; i < N; i++){
+            mp2[B[i]]++;
+        }
+        
+        for(int i = 0; i < N; i++){
+            if(mp1[A[i]]!=mp2[A[i]]){
+                return false;
+            }
+        }
+        return true;
+            
+    }
+};
+
+// { Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        
+        vector<ll> arr(n,0),brr(n,0);
+        
+        // increase the count of elements in first array
+        for(ll i=0;i<n;i++)
+            cin >> arr[i];
+        
+        
+        // iterate through another array
+        // and decrement the count of elements
+        // in the map in which frequency of elements
+        // is stored for first array
+        for(ll i=0;i<n;i++)
+            cin >> brr[i];
+        Solution ob;
+        cout << ob.check(arr,brr,n) << "\n";
+    }
+	return 0;
+}  // } Driver Code Ends
