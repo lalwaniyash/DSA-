fede2d5b95041e0b6c6c1f6d030e557802858ca3
Yash Lalwani <77988031+lalwaniyash@users.noreply.github.com>
2022-06-22 07:36:14 +0000

    Solved "Queue Reversal" from GFG

---
+++ .ccls-cache/@home@runner@DSA/GFG@Queue@Queue Reversal.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+queue<int> rev(queue<int> q);
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    queue<int> a=rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}// } Driver Code Ends
+
+
+//function Template for C++
+
+//Function to reverse the queue.
+queue<int> rev(queue<int> q)
+{
+    stack<int>s;
+    while(!q.empty()){
+        int a = q.front();
+        q.pop();
+        s.push(a);
+    }
+    while(!s.empty()){
+        int a = s.top();
+        s.pop();
+        q.push(a);
+    }
+    return q;
+}
