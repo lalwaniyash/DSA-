eb1ace7ad8ca47dda60cba8099f2ddec94bf62f7
Yash  Lalwani <77988031+lalwaniyash@users.noreply.github.com>
2022-07-03 19:32:29 +0000

    Added "Find duplicates in an array" from gfg

---
+++ .ccls-cache/@home@runner@DSA/GFG@Array@Subarray with given sum.cpp
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        int i=0; 
+        int j=0;
+        vector<int>ans;
+        int sum=0;
+        
+        while(j<n){
+            sum+=arr[j];
+            
+            if(sum>s){
+                while(sum>s){
+                    sum-=arr[i];
+                    i++;
+                }
+            }
+            if(sum == s){
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                return ans;
+            }
+            j++;
+        }
+        return {-1};
+        
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
