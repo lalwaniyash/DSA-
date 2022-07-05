fdc3473e7df783ce5755e3ab44b131dfa9449b64
Yash  Lalwani <77988031+lalwaniyash@users.noreply.github.com>
2022-07-05 06:10:24 +0000

    Added "Cyclically rotate an array by one" from gfg

---
+++ .ccls-cache/@home@runner@DSA/GFG@Array@Find triplets with zero sum.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+ // } Driver Code Ends
+/* You are required to complete the function below
+*  arr[]: input array
+*  n: size of array
+*/
+class Solution{
+  public:
+    //Function to find triplets with zero sum.
+    bool findTriplets(int arr[], int n)
+    { 
+        sort(arr, arr+n);
+        
+        for(int i = 0; i < n; i++){
+            int s = i+1;
+            int e = n-1;
+            while(s<e){
+                if(arr[s]+arr[e] > -arr[i])
+                    e--;
+                else if(arr[s]+arr[e] < -arr[i])
+                    s++;
+                else return true;
+            }
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+	cin>>t;
+	while(t--){
+    	int n;
+    	cin>>n;
+    	int arr[n]={0};
+    	for(int i=0;i<n;i++)
+    		cin>>arr[i];
+    	Solution obj;
+        if(obj.findTriplets(arr, n))
+            cout<<"1"<<endl;
+        else 
+            cout<<"0"<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
