f06345c8b9479a69de350c17ff71ccde25540110
Yash  Lalwani <77988031+lalwaniyash@users.noreply.github.com>
2022-07-05 05:09:01 +0000

    Added "Find Transition Point"  from gfg

---
+++ GFG/Array/Find Transition Point.cpp
@@ -0,0 +1,30 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int transitionPoint(int arr[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cout << transitionPoint(a, n) << endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+int transitionPoint(int arr[], int n) {
+    int i = 0;
+    while(arr[i]!=1 && i<n){
+        i++;
+    }
+    if(i == n) return -1;
+    else return i;
+}
